2adbe21940f2df93207bcb109109811036b8ad53
Chendadev <chenda.net.dev@gmail.com>
2022-05-23 18:27:38 +0200

    create containers MapScreen and FavoritesScreen and displaying on bottom tab navigation

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { Feather } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 // PAGES : 
 import RestaurantsScreen from "./containers/RestaurantsScreen.js";
+import FavoritesScreen from "./containers/FavoritesScreen.js";
+import MapScreen from "./containers/MapScreen.js";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
@@ -19,7 +22,7 @@ function App() {
           headerShown: false,
         }}
       >
-        <Stack.Screen name="Restaurant">
+        <Stack.Screen name="Restaurants">
           {() => (
             <Tab.Navigator
               screenOptions={{
@@ -57,6 +60,65 @@ function App() {
                 )}
               </Tab.Screen>
 
+              {/* BOUTON FAVORIS */}
+              <Tab.Screen
+                name="Favoris"
+                options={{
+                  tabBarLabel: "Favoris",
+                  tabBarIcon: ({ color, size }) => (
+                    <AntDesign name="hearto" size={24} color="black" />
+                  ),
+                }}
+              >
+                {() => (
+                  <Stack.Navigator
+                    screenOptions={{
+                      headerTitle: () => <FavoritesScreen />
+                    }}
+                  >
+
+                    <Stack.Screen
+                      name="Favoris"
+                      options={{
+                        title: "Favoris",
+                      }}
+                    >
+                      {() => <FavoritesScreen />}
+                    </Stack.Screen>
+
+                  </Stack.Navigator>
+                )}
+              </Tab.Screen>
+
+              {/* BOUTON MAP */}
+              <Tab.Screen
+                name="Map"
+                options={{
+                  tabBarLabel: "Map",
+                  tabBarIcon: ({ color, size }) => (
+                    <Feather name="map-pin" size={24} color="black" />
+                  ),
+                }}
+              >
+                {() => (
+                  <Stack.Navigator
+                    screenOptions={{
+                      headerTitle: () => <MapScreen />
+                    }}
+                  >
+
+                    <Stack.Screen
+                      name="Map"
+                      options={{
+                        title: "Map",
+                      }}
+                    >
+                      {() => <MapScreen />}
+                    </Stack.Screen>
+
+                  </Stack.Navigator>
+                )}
+              </Tab.Screen>
             </Tab.Navigator>
           )}
         </Stack.Screen>
